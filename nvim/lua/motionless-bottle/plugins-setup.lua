7d32fd0bd92765efc47a4a753d6cbfa719fdb7e6
Motionless bottle <kashyapkaran2011@gmail.com>
2022-12-22 08:47:45 +0530

    Dotfiles 2022/dec/22
    dotfiles for nvim, kitty, rofi, sway, waybar

---
+++ nvim/lua/motionless-bottle/plugins-setup.lua
@@ -0,0 +1,90 @@
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  
+  -- plugins
+  use 'wbthomason/packer.nvim'
+  
+  -- lua functions that many plugins use
+  use("nvim-lua/plenary.nvim")
+
+  use { "catppuccin/nvim", as = "catppuccin" } -- colorscheme
+
+  use("christoomey/vim-tmux-navigator") --navigation
+  use("szw/vim-maximizer") --maxmize the current window
+
+  -- file explorer
+  use("nvim-tree/nvim-tree.lua")
+
+  -- icons
+  use("kyazdani42/nvim-web-devicons")
+
+  -- status line
+  use("nvim-lualine/lualine.nvim")
+
+  -- fuzzy finding w/ telescope
+  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder- fuzzy finding
+
+  -- autocompletion
+  use("hrsh7th/nvim-cmp")
+  use("hrsh7th/cmp-buffer")
+  use("hrsh7th/cmp-path")
+
+   -- snippets
+  use("L3MON4D3/LuaSnip") -- snippet engine
+  use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+  use("rafamadriz/friendly-snippets") -- useful snippets
+
+  -- managing & installing lsp servers
+  use("williamboman/mason.nvim")
+  use("williamboman/mason-lspconfig.nvim")
+
+  -- configuring lsp servers
+  use("neovim/nvim-lspconfig") -- easily configure language servers
+  use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
+  use({ "glepnir/lspsaga.nvim", branch = "main" }) -- enhanced lsp uis
+  use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+
+  -- formatting and linting
+  use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
+  use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+
+
+  -- treesitter configuration
+use({
+  "nvim-treesitter/nvim-treesitter",
+  run = function()
+    local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+    ts_update()
+  end,
+})
+
+-- auto closing
+use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
+use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
+
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+
